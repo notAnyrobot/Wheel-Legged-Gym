e0e10988d04feff5c3ac048a20f7f4813da1c967
sevenihust <sevenihust@gmail.com>
2024-06-21 13:52:05 +0800

    adds a test script for convincing the user to run the tests.

---
+++ wheel_legged_gym/scripts/test.py
@@ -0,0 +1,30 @@
+import numpy as np
+import os
+from datetime import datetime
+
+import isaacgym
+from wheel_legged_gym.envs import *
+from wheel_legged_gym.utils import get_args, task_registry
+import torch
+
+
+def test(args):
+    # set test environment arguments
+    args.task = "wheel_legged"
+    args.num_envs = 11
+    args.headless = True
+    
+    env, env_cfg = task_registry.make_env(name=args.task, args=args)
+    ppo_runner, train_cfg = task_registry.make_alg_runner(
+        env=env, name=args.task, args=args
+    )
+    task_registry.save_cfgs(name=args.task)
+    ppo_runner.learn(
+        num_learning_iterations=train_cfg.runner.max_iterations,
+        init_at_random_ep_len=True,
+    )
+
+
+if __name__ == "__main__":
+    args = get_args()
+    test(args)
