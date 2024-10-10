dab44d8ab859f19be7641124b90e29fa02d7d0d5
notAnyrobot <liao.shiwen@qq.com>
2024-10-10 17:48:20 +0800

    adds twip robot
    a two wheeled inverted pendulum balancing robot

---
+++ wheel_legged_gym/envs/wheelwalker/twip_config.py
@@ -0,0 +1,48 @@
+from wheel_legged_gym.envs.base.legged_robot_config import (
+    LeggedRobotCfg,
+    LeggedRobotCfgPPO,
+)
+
+
+class TwipCfg(LeggedRobotCfg):
+
+    class env(LeggedRobotCfg.env):
+        num_actions = 2
+
+    class init_state(LeggedRobotCfg.init_state):
+        pos = [0.0, 0.0, 0.5]
+        default_joint_angles = {
+            "left_wheel_joint": 0.0,
+            "right_wheel_joint": 0.0,
+        }
+
+    class control(LeggedRobotCfg.control):
+        pos_action_scale = 0.5
+        vel_action_scale = 10.0
+        stiffness = {
+            "left_wheel_joint": 0.0,
+            "right_wheel_joint": 0.0,
+        }
+        damping = {
+            "left_wheel_joint": 0.5,
+            "right_wheel_joint": 0.5,
+        }
+
+    class asset(LeggedRobotCfg.asset):
+        file = "{WHEEL_LEGGED_GYM_ROOT_DIR}/resources/robots/wheelwalker/urdf/twip.urdf"
+        name = "twip"
+        offset = 0.054
+        l1 = 0.25
+        l2 = 0.25
+        penalize_contacts_on = ["base"]
+        terminate_after_contacts_on = ["base"]
+        self_collisions = 1
+        flip_visual_attachments = False
+
+    class rewards(LeggedRobotCfg.rewards):
+        base_height_target = 0.51
+
+
+class TwipCfgPPO(LeggedRobotCfgPPO):
+    class runner(LeggedRobotCfgPPO.runner):
+        experiment_name = "twip"
